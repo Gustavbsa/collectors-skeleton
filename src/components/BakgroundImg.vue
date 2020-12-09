1a3f495c9d14b87f553c64df6ac3cad2ee8ab168
Gustavbsa <Gustav.Bostrom.5929@student.uu.se>
2020-12-09 12:41:08 +0100

    img on frontpage

---
+++ src/components/BakgroundImg.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="picture">
+      
+</div>
+    
+
+</template>
+
+<script>
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .picture{
+    background-image: url('/images/collectors.jpg');
+    position: absolute;  
+    top: 0px;
+    right: 0px;   
+    width:100%;
+    height:200px;
+    background-repeat: round;
+    border-radius: 4px;
+  }
+</style>
