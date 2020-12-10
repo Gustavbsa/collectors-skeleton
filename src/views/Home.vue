eebfc410778179679f276ef621c7ae11c0267aad
CthomasP <ctparker.se@gmail.com>
2020-12-10 15:28:29 +0100

    delvis klar med info

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,6 +9,7 @@
         </option>
       </select>
       <button v-on:click="start()">Start game</button>
+      <button v-on:click="startInfo()">Game Rules</button>
     </div>
     <div class="form-popup" id="myForm">
     <form class="form-container">
@@ -24,8 +25,26 @@
     <button type="button" class="btn" v-on:click="gameStart('en');">Start Game</button>
     <button type="button" class="btn cancel" v-on:click="closeForm()">Close</button>
   </form></div>
+
+  <div class="info-popup" id="infopop">
+    <form class="info-container">
+    
+    <h1 class="InfoText">Game Rules</h1>
+
+    <input type="page" id="number" value="0"/>
+    <div id="pages"> 
+    </div>
+
+
+    <button type="button" class="btn conitnue" v-on:click="nextInfo()">Continue</button>
+    <button type="button" class="btn back" v-on:click="backInfo()">Back</button>
+    <button type="button" class="btn end" v-on:click="closeInfo()">Close</button>
+  </form>
+
+  </div>
   <div class="picture"><BakgroundImg/></div>
   </div>
+
 </template>
 
 <script>
@@ -37,6 +56,7 @@ export default {
   components:{
     BakgroundImg
   },
+
   created: function () {
     this.$store.commit('SET_ROOM_ID');
   },
@@ -53,6 +73,57 @@ export default {
         playerCount: this.name,
         lang: lang });
       this.$router.push("/room/" + this.$store.state.roomId);
+  },
+startInfo: function(){
+  document.getElementById("infopop").style.display = "block";
+   var value = parseInt(document.getElementById('number').value);
+   value=1;
+   document.getElementById('number').value = value;
+   document.getElementById("pages").innerHTML = "Detta är sida 1"
+
+  
+
+  },
+  nextInfo: function(){
+    var value = parseInt(document.getElementById('number').value);
+    if (value<10){
+      value++;
+    }
+    document.getElementById('number').value = value;
+    if(value==2){
+      document.getElementById("pages").innerHTML = "Detta är sida 2"
+    }else if(value==3){document.getElementById("pages").innerHTML = "Detta är sida 3"}
+    else if(value==4){document.getElementById("pages").innerHTML = "Detta är sida 4"}
+    else if(value==5){document.getElementById("pages").innerHTML = "Detta är sida 5"}
+    else if(value==6){document.getElementById("pages").innerHTML = "Detta är sida 6"}
+    else if(value==7){document.getElementById("pages").innerHTML = "Detta är sida 7"}
+    else if(value==8){document.getElementById("pages").innerHTML = "Detta är sida 8"}
+    else if(value==9){document.getElementById("pages").innerHTML = "Detta är sida 9"}
+    else if(value==10){document.getElementById("pages").innerHTML = "Detta är sida 10"}
+
+  },
+  backInfo: function(){
+    var value = parseInt(document.getElementById('number').value);
+    if (value>1){
+      value--;
+    }
+    document.getElementById('number').value = value;
+    if(value==1){
+      document.getElementById("pages").innerHTML = "Detta är sida 1"
+    }
+    else if(value==2){document.getElementById("pages").innerHTML = "Detta är sida 2"}
+    else if(value==3){document.getElementById("pages").innerHTML = "Detta är sida 3"}
+    else if(value==4){document.getElementById("pages").innerHTML = "Detta är sida 4"}
+    else if(value==5){document.getElementById("pages").innerHTML = "Detta är sida 5"}
+    else if(value==6){document.getElementById("pages").innerHTML = "Detta är sida 6"}
+    else if(value==7){document.getElementById("pages").innerHTML = "Detta är sida 7"}
+    else if(value==8){document.getElementById("pages").innerHTML = "Detta är sida 8"}
+    else if(value==9){document.getElementById("pages").innerHTML = "Detta är sida 9"}
+    else if(value==10){document.getElementById("pages").innerHTML = "Detta är sida 10"}
+
+  },
+  closeInfo: function(){
+    document.getElementById("infopop").style.display = "none";
   }
 }
 }
@@ -103,6 +174,38 @@ export default {
 }
 .PopUpText{
   color: black;
+}
+  .info-popup {
+  display: none;
+  position: fixed;
+  position: center;
+  border: 3px solid #f1f1f1;
+  z-index: 9;
+}
+.info-container .btn {
+  background-color: #4CAF50;
+  color: white;
+  padding: 16px 20px;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+  margin-bottom:10px;
+  opacity: 0.8;
+}
+.info-container .end {
+  background-color: red;
+}
+.info-container {
+  max-width: 600px;
+  padding: 10px;
+  background-color: blue;
+}
+
+.InfoText{
+  color:black;
+}
+.firstpage{
+  color: #4CAF50;
 }
 
 
